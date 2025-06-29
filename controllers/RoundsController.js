3ce2d17dcd41b0213c6550fd1b8af24b2c7189da
GIHAN SHAMIKE <gihanzshamike@gmail.com>
2025-06-29 02:29:07 -0400

    test

--- controllers/RoundsController.js
+++ controllers/RoundsController.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
 
-// Initn Firebase Admin only once
+// Init Firebase Admin only once
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
